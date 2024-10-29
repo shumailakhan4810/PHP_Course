40c994cdba1d239fef01f613e1104b459b5c90fa
shumailakhan4810 <shumaila_sohaib@aptechnorth.edu.pk>
2024-10-29 14:39:13 +0500

    gfgfg

---
+++ basic.php
@@ -0,0 +1,42 @@
+<?php
+echo "HelloWorld" ,"<br>";;
+
+$a= 45;
+$b = 20;
+$mul = $a * $b;
+echo $mul ,"<br>";;
+
+
+//Assignment Operator
+
+$a= 45;
+$b = 20;
+
+$a +=$b;
+echo $a ,"<br>";;
+
+
+$a= 45;
+$b = 20;
+
+$a -=$b;
+echo $a,"<br>";
+$a= 45;
+$b = 20;
+
+$a -=$b;
+echo $a ,"<br>";;
+
+
+$a= 10;
+$b = 2;
+
+$a **=$b;
+echo $a ,"<br>";
+
+$x= 22;
+$y= 45;
+echo  $x!=$y;
+
+
+?>
